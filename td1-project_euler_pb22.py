a7a5a0007a591dfc5e7b9bfc73b8762e00dc7509
VictorKarch <43885792+VictorKarch@users.noreply.github.com>
2018-10-05 20:20:42 +0200

    Update td1-project_euler_pb22.py

--- td1-project_euler_pb22.py
+++ td1-project_euler_pb22.py
@@ -1,8 +1,8 @@
 #Problem 22
 
-def score_name(name):  #On écrit une fonction qui retourne le score d'un nom
+def score_name(name):  #On Ã©crit une fonction qui retourne le score d'un nom
     alphabet=['"','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] 
-    #on génère la liste des lettres latines dans l'ordre alphabétique et la valeur de chaque lettre sera son indice dans la liste (les guillemets comptant pour 0)
+    #on gÃ©nÃ¨re la liste des lettres latines dans l'ordre alphabÃ©tique et la valeur de chaque lettre sera son indice dans la liste (les guillemets comptant pour 0)
     res=0
     for x in name:
         res+=alphabet.index(x)
@@ -10,27 +10,24 @@ def score_name(name):  #On écrit une fonction qui retourne le score d'un nom
  
 #---------------------------------------------------------------------------------------------------
 
-assert score_name("COLIN") == 53 #on vérifie que la fonction renvoie le bon résultat pour l'exemple donné
+assert score_name("COLIN") == 53 #on vÃ©rifie que la fonction renvoie le bon rÃ©sultat pour l'exemple donnÃ©
 
 #---------------------------------------------------------------------------------------------------
-def solve(n): #On écrit la fonction qui calcule le score total d'une liste de noms donnée n
+def solve(n): #On Ã©crit la fonction qui calcule le score total d'une liste de noms donnÃ©e n
     Score_total=0
     for x in n:
-        Score_total+=score_name(x)*(n.index(x)+1)  #on n'oublie pas de décaler les indices de +1
+        Score_total+=score_name(x)*(n.index(x)+1)  #on n'oublie pas de dÃ©caler les indices de +1
     return Score_total
 #---------------------------------------------------------------------------------------------------
 
 
-f = open('p022_names.txt', 'r') #on ouvre le fichier et on créée la liste de noms
+f = open('p022_names.txt', 'r') #on ouvre le fichier et on crÃ©Ã©e la liste de noms
 g=f.read()
-names=g.split(',')  #on sépare les noms au niveau des virgules
-names=sorted(names) #on trie la liste par ordre alphabétique
+names=g.split(',')  #on sÃ©pare les noms au niveau des virgules
+names=sorted(names) #on trie la liste par ordre alphabÃ©tique
 
 #----------------------------------------------------------------------------------------------------
 
-assert names.index('"COLIN"')+1 == 938 #On vérifie que la liste est correctement triée pour le nom COLIN
-
+assert names.index('"COLIN"')+1 == 938 #On vÃ©rifie que la liste est correctement triÃ©e pour le nom COLIN
+#------------------------------------------------------------------------------------------------------
 print(solve(names))
-
-    
-        
