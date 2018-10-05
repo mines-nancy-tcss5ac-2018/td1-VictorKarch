bcedaad558591d5379ae44673819204e3b6a98c8
VictorKarch <43885792+VictorKarch@users.noreply.github.com>
2018-10-05 20:24:41 +0200

    Update td1-project_euler_pb16.py

--- td1-project_euler_pb16.py
+++ td1-project_euler_pb16.py
@@ -1,21 +1,11 @@
-
 #Problem 16
 
 def solve(n):
     res=0    
-    a=str(2**n)    #on transforme le nombre en chaîne de caractères
+    a=str(2**n)    #on transforme le nombre en chaÃ®ne de caractÃ¨res
     for x in a:
-        res+=int(x) #on retransforme chaque caractère chiffré en entier, et on somme le tout dans une boucle
+        res+=int(x) #on retransforme chaque caractÃ¨re chiffrÃ© en entier, et on somme le tout dans une boucle
     return res 
-
-assert solve(15)==26  #on vérifie que cela marche bien pour n=15
-print(solve(1000))  
-        
-
-
- 
-
-
-        
-        
-    
+#--------------------------------------------------------------------------------------------------------
+assert solve(15)==26  #on vÃ©rifie que cela marche bien pour n=15
+print(solve(1000))
